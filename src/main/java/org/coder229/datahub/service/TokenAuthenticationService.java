a9ab3f8d8adeffc1bcc7b6d603e377ecb65264be
Scott Allaire <sallaire4@gmail.com>
2017-12-26 07:50:04 -0600

    Added validation of JWT token contents.

--- src/main/java/org/coder229/datahub/service/TokenAuthenticationService.java
+++ src/main/java/org/coder229/datahub/service/TokenAuthenticationService.java
@@ -2,6 +2,8 @@ package org.coder229.datahub.service;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -15,8 +17,9 @@ import java.util.Date;
 
 @Service
 public class TokenAuthenticationService {
+    protected final Log logger = LogFactory.getLog(getClass());
 
-    @Value("app.security.secret")
+    @Value("${app.security.secret}")
     private String secret;
 
     public void addAuthentication(HttpServletResponse response, String name) {
@@ -24,17 +27,21 @@ public class TokenAuthenticationService {
     }
 
     public Authentication getAuthentication(HttpServletRequest request) {
-        String token = request.getHeader("Authorization");
-        if (token != null) {
-            String user = Jwts.parser()
-                    .setSigningKey(secret)
-                    .parseClaimsJws(token.replace("Bearer", ""))
-                    .getBody()
-                    .getSubject();
+        try {
+            String token = request.getHeader("Authorization");
+            if (token != null) {
+                String user = Jwts.parser()
+                        .setSigningKey(secret)
+                        .parseClaimsJws(token.replace("Bearer", ""))
+                        .getBody()
+                        .getSubject();
 
-            if (user != null) {
-                return new UsernamePasswordAuthenticationToken(user, null, Collections.emptyList());
+                if (user != null) {
+                    return new UsernamePasswordAuthenticationToken(user, null, Collections.emptyList());
+                }
             }
+        } catch (Exception e) {
+            logger.error("Error encountered creating token", e);
         }
 
         return null;
--- src/test/java/org/coder229/datahub/AuthenticationTest.java
+++ src/test/java/org/coder229/datahub/AuthenticationTest.java
@@ -1,13 +1,23 @@
 package org.coder229.datahub;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.coder229.datahub.model.User;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Value;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 
 public class AuthenticationTest extends IntegrationTest {
+
+    @Value("${app.security.secret}")
+    private String secret;
 
     @Test
     public void login() throws Exception {
@@ -15,15 +25,26 @@ public class AuthenticationTest extends IntegrationTest {
 
         ObjectMapper objectMapper = new ObjectMapper();
         String body = objectMapper.writeValueAsString(user);
-        given()
+        ExtractableResponse<Response> response = given()
                 .port(port)
                 .body(body)
-        .when()
+                .when()
                 .post("/login")
-        .then()
+                .then()
                 .statusCode(200)
-        .assertThat()
-                .header("Authorization", notNullValue());
+                .extract();
+
+        String authorization = response.header("Authorization");
+        assertThat(authorization, notNullValue());
+        String[] parts = authorization.split(" ");
+        assertThat(parts[0], equalTo("Bearer"));
+
+        Jws<Claims> claims = Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(parts[1]);
+        assertThat(claims, notNullValue());
+        assertThat(claims.getBody().getSubject(), equalTo("admin"));
+        assertThat(claims.getBody().getExpiration(), notNullValue());
     }
 
 }
