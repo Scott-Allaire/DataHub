d8e2f0c8d8031273867a61581ab66b0bdc4ad669
Scott Allaire <sallaire4@gmail.com>
2019-07-04 11:47:02 -0500

    Move message handling to separate function

--- helpers/queueHelper.js
+++ helpers/queueHelper.js
@@ -21,6 +21,47 @@ function insertReading(conn, code, source, value, epoch) {
     });
 }
 
+function handleMessage(topic, message) {
+    const source = topic.indexOf('/') >= 0 ? topic.split('/')[1] : topic;
+
+    if (topic.indexOf('camera') >= 0) {
+        console.log('Received image from ' + source);
+    } else {
+        console.log('Received: ' + message.toString() + ' from ' + source);
+        const json = JSON.parse(message);
+
+        pool.getConnection((err, conn) => {
+            if (json.tempf != null) {
+                insertReading(conn, 'tempf', source, json.tempf, json.epoch);
+            }
+            if (json.tempc != null) {
+                insertReading(conn, 'tempc', source, json.tempc, json.epoch);
+            }
+            if (json.hum != null) {
+                insertReading(conn, 'hum', source, json.hum, json.epoch);
+            }
+            if (json.humidity != null) {
+                insertReading(conn, 'humidity', source, json.humidity, json.epoch);
+            }
+            if (json.feels_like != null) {
+                insertReading(conn, 'feels_like', source, json.feels_like, json.epoch);
+            }
+            if (json.pressure != null) {
+                insertReading(conn, 'pressure', source, json.pressure, json.epoch);
+            }
+            if (json.lowbatt != null) {
+                insertReading(conn, 'lowbatt', source, json.lowbatt, json.epoch);
+            }
+            if (json.light != null) {
+                insertReading(conn, 'light', source, json.light, json.epoch);
+            }
+            if (json.airquality != null) {
+                insertReading(conn, 'airquality', source, json.airquality, json.epoch);
+            }
+        });
+    }
+}
+
 const init = () => {
     console.log("Attempting to connect to MQTT server at " + brokerUrl);
     const client = mqtt.connect(brokerUrl, brokerOptions);
@@ -35,51 +76,12 @@ const init = () => {
     });
 
     client.on('message', (topic, message) => {
-        const source = topic.indexOf('/') >= 0 ? topic.split('/')[1] : topic;
-
-        if (topic.indexOf('camera') >= 0) {
-            console.log('Received image from ' + source);
-        } else {
-            console.log('Received: ' + message.toString() + ' from ' + source);
-
-            try {
-                const json = JSON.parse(message);
-
-                pool.getConnection((err, conn) => {
-                    if (json.tempf != null) {
-                        insertReading(conn, 'tempf', source, json.tempf, json.epoch);
-                    }
-                    if (json.tempc != null) {
-                        insertReading(conn, 'tempc', source, json.tempc, json.epoch);
-                    }
-                    if (json.hum != null) {
-                        insertReading(conn, 'hum', source, json.hum, json.epoch);
-                    }
-                    if (json.humidity != null) {
-                        insertReading(conn, 'humidity', source, json.humidity, json.epoch);
-                    }
-                    if (json.feels_like != null) {
-                        insertReading(conn, 'feels_like', source, json.feels_like, json.epoch);
-                    }
-                    if (json.pressure != null) {
-                        insertReading(conn, 'pressure', source, json.pressure, json.epoch);
-                    }
-                    if (json.lowbatt != null) {
-                        insertReading(conn, 'lowbatt', source, json.lowbatt, json.epoch);
-                    }
-                    if (json.light != null) {
-                        insertReading(conn, 'light', source, json.light, json.epoch);
-                    }
-                    if (json.airquality != null) {
-                        insertReading(conn, 'airquality', source, json.airquality, json.epoch);
-                    }
-                });
-            }
-            catch(err) {
-                console.log("Error parsing document", err);
-            }
+        try {
+            handleMessage(topic, message);
+        } catch (err) {
+            console.log("Error parsing document", err);
         }
-    })
+    });
 };
 
 module.exports = {
